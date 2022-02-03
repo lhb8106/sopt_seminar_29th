e8cad488789d2c291991e29bb9c3426063696709
lhb8106 <lhb8106@naver.com>
2022-02-03 23:13:37 +0900

    [FEAT] level1 end

--- app/src/main/java/com/example/soptseminar29th/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/example/soptseminar29th/presentation/home/HomeActivity.kt
@@ -1,15 +1,11 @@
 package com.example.soptseminar29th.presentation.home
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.core.view.isVisible
 import com.example.soptseminar29th.R
 import com.example.soptseminar29th.databinding.ActivityHomeBinding
 import com.example.soptseminar29th.presentation.home.viewmodel.HomeViewModel
 import com.example.soptseminar29th.util.BaseViewUtil
-import com.example.soptseminar29th.util.changeFragment
 
 class HomeActivity
     : BaseViewUtil.BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
@@ -17,16 +13,27 @@ class HomeActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
+        initViewSetting()
     }
 
     private fun initViewModel() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+    }
 
+    private fun initViewSetting() {
         val followerFragment = FollowerFragment()
         val repositstoryFragment = RepositoryFragment()
 
         supportFragmentManager.beginTransaction().add(R.id.container_rv, followerFragment).commit()
+
+        binding.tvFollower.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.container_rv, followerFragment).commit()
+        }
+
+        binding.tvRepository.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.container_rv, repositstoryFragment).commit()
+        }
     }
 
 }
--- app/src/main/java/com/example/soptseminar29th/presentation/home/RepositoryFragment.kt
+++ app/src/main/java/com/example/soptseminar29th/presentation/home/RepositoryFragment.kt
@@ -6,13 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.soptseminar29th.R
+import com.example.soptseminar29th.data.FollowerData
+import com.example.soptseminar29th.data.RepositoryData
+import com.example.soptseminar29th.databinding.FragmentFollowerBinding
+import com.example.soptseminar29th.databinding.FragmentRepositoryBinding
+import com.example.soptseminar29th.util.BaseViewUtil
 
-class RepositoryFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repository, container, false)
+class RepositoryFragment :
+    BaseViewUtil.BaseFragment<FragmentRepositoryBinding>(R.layout.fragment_repository) {
+    private lateinit var repositoryAdapter: RepositoryAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+
+    }
+
+    private fun initAdapter() {
+        repositoryAdapter = RepositoryAdapter()
+        binding.rvRepository.adapter = repositoryAdapter
+        repositoryAdapter.setRepositoryData(
+            mutableListOf(
+                RepositoryData("이혜빈1", "123"),
+                RepositoryData("이혜빈2", "1234"),
+                RepositoryData("이혜빈3", "12345"),
+            )
+        )
     }
 }
